6bce2f24a3d07d5ef67374835eb6f34aeb86e067
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-03-18 19:54:57 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -25,12 +25,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://bit.ly/3JsnKHM')
+                print('https://bit.ly/3ucp22W')
                 return
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://bit.ly/3JsnKHM')
+                print('https://bit.ly/3ucp22W')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
