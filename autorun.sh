6af6d955ed297cdcd4995a324bdefd7c43ede693
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-20 23:08:20 -0300

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -10,13 +10,3 @@ python youtube_m3ugrabber.py > ../youtube.m3u
 
 echo m3u grabbed
 
-echo $(dirname $0)
-
-python -m pip install requests
-
-cd $(dirname $0)/scripts/
-
-python youtube_m3ugrabber.py > ../youtube_chile.m3u
-
-echo m3u grabbed
-
