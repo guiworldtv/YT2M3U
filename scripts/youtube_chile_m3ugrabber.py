e570e6e423d359d5de4d11bf00501181c7ba71fb
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-20 22:58:06 -0300

    Update youtube_chile_m3ugrabber.py

--- scripts/youtube_chile_m3ugrabber.py
+++ scripts/youtube_chile_m3ugrabber.py
@@ -1,42 +1,17 @@
 #! /usr/bin/python3
-
-banner = r'''
-###########################################################################
-#                                                                         #
-
-#                                  >> https://github.com/guiworldtv       #
-###########################################################################
-
-
-
-'''
-
 import requests
 import os
-import sys
-
-windows = False
-if 'win' in sys.platform:
-    windows = True
 
 def grab(url):
-    response = requests.get(url, timeout=15).text
+    response = requests.get(url, timeout = 15).text
     if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://eu-nl-012.worldcast.tv/dancetelevisionone/2/dancetelevisionone.m3u8')
-                return
-            os.system(f'wget {url} -O temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://eu-nl-012.worldcast.tv/dancetelevisionone/2/dancetelevisionone.m3u8')
-                return
+        print("https://www.youtube.com/watch?v=1oh9IEwBbFY")
+        return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
+        if 'https://' in response[end - tuner : end]:
+            link = response[end - tuner : end]
             start = link.find('https://')
             end = link.find('.m3u8') + 5
             break
@@ -44,22 +19,16 @@ def grab(url):
             tuner += 5
     print(f"{link[start : end]}")
 
-print('#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"')
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
-with open('../youtube_info_chile.txt', errors="ignore") as f:
+with open('./youtubeLink.txt', encoding='utf-8') as f:
     for line in f:
         line = line.strip()
-        if not line or line.startswith('~~'):
+        if not line or line.startswith('##'):
             continue
         if not line.startswith('https:'):
-            line = line.split('|')
+            line = line.split('-')
             ch_name = line[0].strip()
             grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
+            print(f'\n#EXTINF:-1 group-title="{grp_title}", {ch_name}')
         else:
             grab(line)
             
