73991fb9aac75fbf9c1d8b3b0b9e5c8c4941ff96
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-06-20 22:26:51 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -67,7 +67,44 @@ if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')
     
-    with open('../youtube_info_chile.txt', errors="ignore") as f:
+import requests
+import os
+import sys
+
+windows = False
+if 'win' in sys.platform:
+    windows = True
+
+def grab(url):
+    response = requests.get(url, timeout=15).text
+    if '.m3u8' not in response:
+        #response = requests.get(url).text
+        if '.m3u8' not in response:
+            if windows:
+                print('https://eu-nl-012.worldcast.tv/dancetelevisionone/2/dancetelevisionone.m3u8')
+                return
+            os.system(f'wget {url} -O temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print('https://eu-nl-012.worldcast.tv/dancetelevisionone/2/dancetelevisionone.m3u8')
+                return
+    end = response.find('.m3u8') + 5
+    tuner = 100
+    while True:
+        if 'https://' in response[end-tuner : end]:
+            link = response[end-tuner : end]
+            start = link.find('https://')
+            end = link.find('.m3u8') + 5
+            break
+        else:
+            tuner += 5
+    print(f"{link[start : end]}")
+
+print('#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"')
+print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
+print(banner)
+#s = requests.Session()
+with open('../youtube_info_chile.txt', errors="ignore") as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
