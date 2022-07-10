72c7dce8382cdf9f1dd70986be1bdf238188d25f
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-10 07:37:19 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -1,66 +1,111 @@
-#! /usr/bin/python3
-
-banner = r'''
-###########################################################################
-#                                  >> https://github.com/guiworldtv       #
-###########################################################################
-
-
-'''
-
 import requests
-import os
-import sys
+import re
+import json
+import urllib
+import control
 
-windows = False
-if 'win' in sys.platform:
-    windows = True
+proxy = control.proxy_url
+proxy = None if proxy is None or proxy == '' else {
+    'http': proxy,
+    'https': proxy,
+}
 
-def grab(url):
-    response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAirYT.m3u8')
-                return
-            os.system(f'wget {url} -O temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAirYT.m3u8')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
 
-print('#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"')
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
-with open('../youtube_channel_info.txt', errors="ignore") as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
-        else:
-            grab(line)
-            
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+def geturl(url):
 
+    control.log('YouTube: %s' % url)
+
+    if url.startswith('https://www.youtube.com/embed/live_stream?channel='):
+        video_id = get_video_id(url)
+        # return get_manifest_url(video_id)
+        return get_manifest_url_v2(video_id)
+
+    r = re.search(r'https?://www.youtube.com/embed/([^/|^?]+)', url)
+    if r:
+        video_id = r.group(1)
+        url = get_manifest_url(video_id)
+
+        if not url:
+            url = get_manifest_url_v2(video_id)
+
+        return url
+
+    if url and not url.startswith('http'):
+        url = 'https://www.youtube.com/channel/{channel}/live'.format(channel=url)
+
+    # https://www.youtube.com/channel/UCoMdktPbSTixAyNGwb-UYkQ/live
+    if url.startswith('https://www.youtube.com/channel/'):
+        if not url.endswith('/live'):
+            url = url + '/live'
+        channel = re.search(r'https://www.youtube.com/channel/(.*?)/live', url).group(1)
+        video_id = get_video_id('https://www.youtube.com/embed/live_stream?channel=' + channel)
+
+        manifest_url = get_manifest_url(video_id)
+
+        return manifest_url
+
+    webpage = requests.get(url, proxies=proxy).content
+    mobj = re.search(r';ytplayer.config = ({.*?});', webpage)
+    player_config = json.loads(mobj.group(1))
+
+    player_response_string = player_config['args']['player_response']
+
+    player_response = json.loads(player_response_string)
+
+    return player_response['streamingData']['hlsManifestUrl']
+
+
+# https://www.youtube.com/embed/live_stream?channel=UCuiLR4p6wQ3xLEm15pEn1Xw
+# url = 'https://www.youtube.com/embed/live_stream?channel=' + channel
+def get_video_id(url):
+
+    webpage = requests.get(url, proxies=proxy).content
+
+    return re.search(r'\"video_id\":\"(.*?)\"', webpage).group(1)
+
+
+def get_manifest_url(video_id):
+
+    url = 'https://www.youtube.com/get_video_info?video_id=' + video_id
+    result = requests.get(url, proxies=proxy).content
+    result = urllib.unquote_plus(result)
+
+    result = re.search(r'\"hlsManifestUrl\":\"(.*?)\"', result)
+
+    if not result:
+        control.log('NO hlsManifestUrl', control.LOGERROR)
+
+    return result.group(1) if result else None
+
+
+def get_manifest_url_v2(video_id):
+
+    url = 'https://www.youtube.com/watch?v=%s&gl=US&hl=en&has_verified=1&bpctr=9999999999' % video_id
+    result = requests.get(url, proxies=proxy).content
+
+    # print result
+
+    result = re.search(r'var ytInitialPlayerResponse\s*=\s*({.+?});', result)
+
+    if not result:
+        print('NO Config')
+        return None
+
+    config = result.group(1)
+
+    config_json = json.loads(config)
+
+    return config_json['streamingData']['hlsManifestUrl']
+
+
+def get_manifest_url_by_url(url):
+
+    result = requests.get(url, proxies=proxy).content
+    result = urllib.unquote_plus(result)
+
+    result = re.search(r'\"hlsManifestUrl\":\"(.*?)\"', result)
+
+    if not result:
+        control.log('NO hlsManifestUrl', control.LOGERROR)
+
+    return result.group(1) if result else None
