ce0248f5dd90ef809458122ad4c1fbd670072633
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-01-24 16:58:18 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -10,22 +10,15 @@ banner = r'''
 
 import requests
 import os
-import sys
-
-windows = False
-if 'win' in sys.platform:
-    windows = True
 
 def grab(url):
     response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAirYT.m3u8')
-                return
-            os.system(f'wget {url} -O temp.txt')
-            response = ''.join(open('temp.txt').readlines())
+        with open("temp.txt", "w") as f:
+            response = requests.get(url).text
+            f.write(response)
+        with open("temp.txt", "r") as f:
+            response = f.read()
             if '.m3u8' not in response:
                 print('https://raw.githubusercontent.com/guiworldtv/MEU-IPTV-FULL/main/VideoOFFAirYT.m3u8')
                 return
@@ -43,9 +36,20 @@ def grab(url):
 
 print('#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"')
 print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
-with open('../youtube_channel_info.txt', errors="ignore") as f:
+
+class Channel:
+    def __init__(self, ch_name, grp_title, tvg_logo, tvg_id):
+        self.ch_name = ch_name
+        self.grp_title = grp_title
+        self.tvg_logo = tvg_logo
+        self.tvg_id = tvg_id
+
+    def __str__(self):
+        return f'\n#EXTINF:-1 group-title="{self.grp_title}" tvg-logo="{self.tvg_logo}" tvg-id="{self.tvg_id}", {self.ch_name}'
+
+channels = []
+
+with open("youtube_channel_info.txt", errors="ignore") as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
@@ -56,10 +60,13 @@ with open('../youtube_channel_info.txt', errors="ignore") as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
+            channels.append(Channel(ch_name, grp_title, tvg_logo, tvg_id))
         else:
             grab(line)
-            
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+
+for channel in channels:
+    print(channel)
+
+if os.path.exists("temp.txt"):
+    os.remove("temp.txt")
+    os.remove("watch*")
